b29f79d542b12d04fe6a4bcf6cdb93a827a9f3ae
LINTANF Thomas <thomas.lintanf@renault.com>
2019-06-20 12:21:57 +0200

    Création de la classe pentamino et de preparation  de la liste

---
+++ main.py
@@ -0,0 +1,8 @@
+from pentamino import cPentamino
+from grid import cGrid
+
+lSymboles = ['A','B','C','D','E','F','G','H','I','J','K','L']
+lPentaminos = []
+
+for symbole in lSymboles:
+    lPentaminos.append(cPentamino(symbole))
---
+++ pentamino.py
@@ -0,0 +1,5 @@
+class cPentamino:
+
+    def __init__(self,symbole):
+        self.symoble = symbole
+        self.cases = [[0,1],[0,2],[0,3],[0,4]]
